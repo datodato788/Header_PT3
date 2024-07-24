1b51c1b2fad042d4c3bb3957f9106e11059aeba3
datodato788 <151830320+datodato788@users.noreply.github.com>
2024-07-24 14:57:18 +0400

    3

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
 const nav_2 = document.getElementById("nav_2");
 
 function menus() {
-  nav_2.classList.add("nav_2_none");
-  nav_2.classList.toggle("nav_2_show")
+  nav_2.classList.toggle("nav_2_show");
 }
